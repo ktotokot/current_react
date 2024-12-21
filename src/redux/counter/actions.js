c92c4f2c47f5d7354ebfae8483f763efd982ac61
ktotokot <shabuninae@gmail.com>
2024-12-21 17:24:03 +0200

    redux1

---
+++ src/redux/counter/actions.js
@@ -0,0 +1,19 @@
+// export const increment = () => {
+//   return { type: "INCREMENT" };
+// };
+// export const decrement = () => {
+//   return { type: "DECREMENT" };
+// };
+// export const reset = () => {
+//   return { type: "RESET" };
+// };
+// export const changeStep = (step) => {
+//   return { type: "CHANGE_STEP", payload: step };
+// };
+
+import { createAction } from "@reduxjs/toolkit";
+
+export const increment = createAction("increment");
+export const decrement = createAction("decrement");
+export const reset = createAction("reset");
+export const changeStep = createAction("changeStep");
